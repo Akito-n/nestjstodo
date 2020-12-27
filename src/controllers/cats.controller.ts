6f383cf8a94c9f23c35927bd6ed46a609d6c82fb
Akito-n <a.nagai@mof-mof.co.jp>
2020-12-27 15:59:46 +0900

    class-validator

--- src/controllers/cats.controller.ts
+++ src/controllers/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, ParseIntPipe} from '@nestjs/common'
+import { Controller, Get, Post, Body, Param, ParseIntPipe, UsePipes, ValidationPipe} from '@nestjs/common'
 import { CreateCatDto } from '../dto/create-cat.dto'
 import {CatsService } from '../services/CatsService'
 import { Cat } from '../interfaces/cat.interface'
@@ -15,7 +15,7 @@ async findOne(@Param('id', ParseIntPipe) id: number) {
 }
 
   @Post()
-  async create(@Body() createCatDto :CreateCatDto){
+  async create(@Body(new ValidationPipe()) createCatDto :CreateCatDto){
     this.catsService.create(createCatDto)
   }
 
@@ -23,4 +23,3 @@ async findOne(@Param('id', ParseIntPipe) id: number) {
   async findAll(): Promise<Cat[]> {
     return this.catsService.findAll()
   }
-}
--- src/validation.pipe.ts
+++ src/validation.pipe.ts
@@ -1,9 +1,26 @@
-import { PipeTransform, Injectable, ArgumentMetadata} from '@nestjs/common'
+import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException} from '@nestjs/common'
+import { plainToClass } from 'class-transformer'
+import { Validate } from 'class-validator'
 
 @Injectable()
-export class ValidationPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata){
-    return value
+export class ValidationPipe implements PipeTransform<any> {
+  async transform(value: any, {metatype}: ArgumentMetadata){
+    if(!metatype || this.toValidate(metatype)){
+      return value
+    }
+
+    const object = plainToClass(metatype, value)
+    const errors = await Validate(object)
+
+    if(errors.length > 0){
+      throw new BadRequestException('validation Faild')
+    }
+      return value
+  }
+
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object]
+    return !types.includes(metatype)
   }
 }
 
